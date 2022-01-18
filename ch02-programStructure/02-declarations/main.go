3eed1121666e202721257779e12e4d3eed0f5b60
tinmanjk <tinmanjk@mail.com>
2022-01-18 23:27:36 +0200

    ch02: function declaration

--- ch02-programStructure/02-declarations/main.go
+++ ch02-programStructure/02-declarations/main.go
@@ -12,4 +12,13 @@ func main() {
 	// %g -> general -> gives you the shorter of
 	// %e (scientific) or %f (fixed point)
 	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
+
+	const freezingF = 32.0                                  // function level constant
+	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
+	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))   // "212°F = 100°C"
+}
+
+// function declaration
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
 }
