20ae2b5cdb6b921b20ac1c42189474843cfe0b98
tinmanjk <tinmanjk@mail.com>
2022-01-19 17:01:59 +0200

    ch02: aliasing tempconv package name to tc

--- ch02-programStructure/06-packagesAndFiles/main.go
+++ ch02-programStructure/06-packagesAndFiles/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/tinmanjk/tgpl/ch02-programStructure/06-packagesAndFiles/tempconv"
+	tc "github.com/tinmanjk/tgpl/ch02-programStructure/06-packagesAndFiles/tempconv"
 )
 
 func main() {
 	// because of the String method it prints "°C" suffix on the constant of the type Celsius
-	fmt.Printf("Brrrr! %v\n", tempconv.AbsoluteZeroC) // "Brrrr! -273.15°C"
-	fmt.Println(tempconv.CToF(tempconv.BoilingC))     // "212°F"
+	fmt.Printf("Brrrr! %v\n", tc.AbsoluteZeroC) // "Brrrr! -273.15°C"
+	fmt.Println(tc.CToF(tc.BoilingC))           // "212°F"
 
 	fmt.Println("2.6.1 Imports")
 	// 1. Import paths -> "github.com/tinmanjk/tgpl/ch02-programStructure/06-packagesAndFiles/tempconv"
