ef907e9741245c07ab1b3a2499dfb7d97aa386ae
tinmanjk <tinmanjk@mail.com>
2022-02-11 02:12:24 +0200

    ch07: simple routing, url.query and 404 handling

--- ch07-interfaces/07-thehttpHandlerInterface/main.go
+++ ch07-interfaces/07-thehttpHandlerInterface/main.go
@@ -32,7 +32,25 @@ type database map[string]dollars
 // logic for EVERY http request
 func (db database) ServeHTTP(w http.ResponseWriter, // type response struct
 	req *http.Request) {
-	for item, price := range db {
-		fmt.Fprintf(w, "%s: %s\n", item, price)
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound) // 404
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+	default:
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such page: %s\n", req.URL)
+		// alternatively
+		// msg := fmt.Sprintf("no such page: %s\n", req.URL)
+		// http.Error(w, msg, http.StatusNotFound) // 404
 	}
 }
