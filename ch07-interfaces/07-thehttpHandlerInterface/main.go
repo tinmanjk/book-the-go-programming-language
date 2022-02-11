b64c44c88d06128030c4c1a835c41eb36545a70e
tinmanjk <tinmanjk@mail.com>
2022-02-11 02:41:55 +0200

    ch07: servemux - request multiplexer

--- ch07-interfaces/07-thehttpHandlerInterface/main.go
+++ ch07-interfaces/07-thehttpHandlerInterface/main.go
@@ -6,13 +6,46 @@ import (
 	"net/http"
 )
 
+const useMux = true
+
+var dbHandlesListOnly bool
+
 func main() {
 	fmt.Println("The HTTP Handler Interface")
 
 	db := database{"shoes": 50, "socks": 5}
 
+	// ServeMux -> aggregates different Handlers into one
+	// simplifies routing (association urls and handlers)
+	mux := http.NewServeMux() // request multiplexer -> http.Handler
+	mux.Handle("/list", db)   // normal handler (adapted, db.list originally in book)
+
+	// func type adapter
+	// 	type HandlerFunc func(ResponseWriter, *Request)
+
+	// func (f HandlerFunc) ServeHTTP(w ResponseWriter, r *Request) {
+	// 	f(w, r)
+	// }
+	// **let's a function value satisfy an interface**
+	mux.Handle("/price", http.HandlerFunc(db.price)) // **conversion** of method value
+	// mux.HandleFunc("/price", db.price) -> CONVENIENCE METHOD
+
+	// http.Handler satisfaction by serve mux
+	//func (mux *ServeMux) ServeHTTP(w ResponseWriter, r *Request) {
+	// ...
+	// 	h, _ := mux.Handler(r)
+	// 	h.ServeHTTP(w, r)
+	// }
+
 	// func ListenAndServe(address string, h Handler) error
-	log.Fatal(http.ListenAndServe("localhost:8000", db))
+	if useMux {
+		dbHandlesListOnly = true
+		log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	} else {
+		dbHandlesListOnly = false
+		log.Fatal(http.ListenAndServe("localhost:8000", db))
+
+	}
 }
 
 type dollars float32
@@ -29,9 +62,14 @@ func (d dollars) String() string {
 
 type database map[string]dollars
 
-// logic for EVERY http request
 func (db database) ServeHTTP(w http.ResponseWriter, // type response struct
 	req *http.Request) {
+
+	if dbHandlesListOnly {
+		db.list(w, req)
+		return
+	}
+
 	switch req.URL.Path {
 	case "/list":
 		for item, price := range db {
@@ -53,4 +91,22 @@ func (db database) ServeHTTP(w http.ResponseWriter, // type response struct
 		// msg := fmt.Sprintf("no such page: %s\n", req.URL)
 		// http.Error(w, msg, http.StatusNotFound) // 404
 	}
+}
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter,
+	req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
 }
