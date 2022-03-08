c9bb72ad1dd01c854af2e0d6081e4032c4bd7c1c
tinmanjk <tinmanjk@mail.com>
2022-03-08 22:21:03 +0200

    ch07: example - sequential time server and simple netcat implementation

---
+++ ch08-goroutinesAndChannels/02-exampleConcurrentClockServer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	fmt.Println("Example: Concurrent Clock Server")
+
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+
+		conn, err := listener.Accept() // blocks until a connection is made
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		handleConn(conn) // blocks -> handle one connection at a time ->
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
