4ade6853bff2bb810406687cfa5e596064c113ce
tinmanjk <tinmanjk@mail.com>
2022-03-08 21:45:13 +0200

    ch08: main goroutine terminates - all goroutines terminate

---
+++ ch08-goroutinesAndChannels/01-goroutines/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Goroutines")
+
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+	// when the main goroutine exists -> it terminates the sub-goroutines
+	// much the same as background threads
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
